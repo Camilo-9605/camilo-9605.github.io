dc122b43bb7c15844f89758a086c5f03c710aba1
Juan Camilo Holguin <camiloholguin9605@gmail.com>
2026-02-01 18:55:49 -0500

    actualizando archivos html-js

--- script.js
+++ script.js
@@ -5,5 +5,14 @@ hamburger.addEventListener('click', () => {
   navList.classList.toggle('active');
 });
 
+const button = document.getElementById("openModal");
+const socialIcons = document.getElementById("socialIcons");
 
+button.addEventListener("click", () => {
+  if (socialIcons.style.display === "none") {
+    socialIcons.style.display = "block";
+  } else {
+    socialIcons.style.display = "none";
+  }
+});
 
